2603ded486e25341b8f8b3e6fac1421b2bcfa83a
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-02-25 11:40:14 +0200

    update: started user

--- src/main/java/com/example/virtualwallet/models/User.java
+++ src/main/java/com/example/virtualwallet/models/User.java
@@ -1,4 +1,37 @@
 package com.example.virtualwallet.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
 }
