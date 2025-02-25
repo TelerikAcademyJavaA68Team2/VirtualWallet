baf77447b7a63bd3b0f73b470d5ccdf0eba16c75
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-25 23:58:26 +0200

    adding basic TransactionService impl

---
+++ src/main/java/com/example/virtualwallet/services/TransactionServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.virtualwallet.services;
+
+import com.example.virtualwallet.models.Transaction;
+import com.example.virtualwallet.repositories.TransactionRepository;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class TransactionServiceImpl implements TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionServiceImpl(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+
+    @Override
+    public void createTransaction(Transaction transaction) {
+        transactionRepository.save(transaction);
+    }
+
+    @Override
+    public Set<Transaction> findAllTransactionsByUserId(UUID userId) {
+        return transactionRepository.findAllTransactionsByUserId(userId);
+    }
+
+    @Override
+    public Set<Transaction> findAllTransactionsByWalletId(UUID walletId) {
+        return transactionRepository.findAllTransactionsByWalletId(walletId);
+    }
+}
