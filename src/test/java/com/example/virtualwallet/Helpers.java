4b4badec81fe395aca3963b2d6c102d7be297035
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-26 20:48:34 +0200

    added basic test Helpers

---
+++ src/test/java/com/example/virtualwallet/Helpers.java
@@ -0,0 +1,47 @@
+package com.example.virtualwallet;
+
+import com.example.virtualwallet.models.User;
+import com.example.virtualwallet.models.enums.MainCurrency;
+import com.example.virtualwallet.models.enums.Role;
+
+import java.util.UUID;
+
+public class Helpers {
+
+    public static final String MOCK_USER_FIRST_NAME = "MockFirstNameUser";
+    public static final String MOCK_ADMIN_FIRST_NAME = "MockFirstNameAdmin";
+    public static final String MOCK_USER_LAST_NAME = "MockLastNameUser";
+    public static final String MOCK_ADMIN_LAST_NAME = "MockLastNameAdmin";
+    public static final String MOCK_USER_USERNAME = "MockUsername";
+    public static final String MOCK_ADMIN_USERNAME = "MockAdminName";
+    public static final String MOCK_PASSWORD = "MockPassword";
+    public static final String MOCK_USER_EMAIL = "mock@user.com";
+    public static final String MOCK_ADMIN_EMAIL = "mock@admin.com";
+
+
+    public static User createMockUser() {
+        var mockUser = new User();
+        mockUser.setId(UUID.randomUUID());
+        mockUser.setFirstName(MOCK_USER_FIRST_NAME);
+        mockUser.setLastName(MOCK_USER_LAST_NAME);
+        mockUser.setEmail(MOCK_USER_EMAIL);
+        mockUser.setUsername(MOCK_USER_USERNAME);
+        mockUser.setPassword(MOCK_PASSWORD);
+        mockUser.setRole(Role.USER);
+        mockUser.setMainCurrency(MainCurrency.BGN);
+        return mockUser;
+    }
+
+    public static User createMockAdmin() {
+        var mockUser = new User();
+        mockUser.setId(UUID.randomUUID());
+        mockUser.setFirstName(MOCK_ADMIN_FIRST_NAME);
+        mockUser.setLastName(MOCK_ADMIN_LAST_NAME);
+        mockUser.setEmail(MOCK_ADMIN_EMAIL);
+        mockUser.setUsername(MOCK_ADMIN_USERNAME);
+        mockUser.setPassword(MOCK_PASSWORD);
+        mockUser.setRole(Role.ADMIN);
+        mockUser.setMainCurrency(MainCurrency.BGN);
+        return mockUser;
+    }
+}
