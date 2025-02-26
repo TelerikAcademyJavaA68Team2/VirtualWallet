269ceded4177e03191172cbf141dccce269e5127
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-26 19:18:15 +0200

    changed ModelMapper for mapping to UserOutput from Object

--- src/main/java/com/example/virtualwallet/helpers/ModelMapper.java
+++ src/main/java/com/example/virtualwallet/helpers/ModelMapper.java
@@ -1,16 +1,22 @@
 package com.example.virtualwallet.helpers;
 
 import com.example.virtualwallet.models.Dtos.UserOutput;
-import com.example.virtualwallet.models.User;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ModelMapper {
 
-    public UserOutput mapUserToUserOutput(User user){
-        UserOutput userOutput = new UserOutput();
+    public UserOutput mapObjectToUserOutput(Object[] userObject) {
+        return new UserOutput((String) userObject[0],
+                (String) userObject[1],
+                (String) userObject[2],
+                (String) userObject[3],
+                (Boolean) userObject[4],
+                ((Number) userObject[5]).intValue());
+    }
 
-
-        return userOutput;
+    public Page<UserOutput> mapObjectPageToUserOutputPage(Page<Object[]> usersObjectArray) {
+        return usersObjectArray.map(this::mapObjectToUserOutput);
     }
 }
