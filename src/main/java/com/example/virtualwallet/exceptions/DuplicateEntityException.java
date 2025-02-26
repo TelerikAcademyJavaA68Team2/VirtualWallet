cca6b8635d9bd108bfea56372f59851ea5e9ffe8
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-26 15:00:24 +0200

    update exception constructors

--- src/main/java/com/example/virtualwallet/exceptions/DuplicateEntityException.java
+++ src/main/java/com/example/virtualwallet/exceptions/DuplicateEntityException.java
@@ -1,7 +1,11 @@
 package com.example.virtualwallet.exceptions;
 
 public class DuplicateEntityException extends RuntimeException {
-    public DuplicateEntityException(String message) {
-        super(message);
+    public DuplicateEntityException(String type) {
+        super(String.format("%s already exists.", type));
+    }
+
+    public DuplicateEntityException(String type, String attribute) {
+        super(String.format("%s with %s already exists.", type, attribute));
     }
 }
--- src/main/java/com/example/virtualwallet/exceptions/EntityNotFoundException.java
+++ src/main/java/com/example/virtualwallet/exceptions/EntityNotFoundException.java
@@ -1,7 +1,13 @@
 package com.example.virtualwallet.exceptions;
 
+import java.util.UUID;
+
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
+    public EntityNotFoundException(String type) {
+        super(String.format("%s not found.", type));
+    }
+
+    public EntityNotFoundException(String type, UUID id) {
+        super(String.format("%s with id %s not found.", type, id));
     }
 }
