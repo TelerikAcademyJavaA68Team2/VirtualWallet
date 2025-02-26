e9e12c2cc60a7fd0ac3ebc7d303bba489d561017
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-26 19:14:25 +0200

    implementing UserOutput

--- src/main/java/com/example/virtualwallet/models/Dtos/UserOutput.java
+++ src/main/java/com/example/virtualwallet/models/Dtos/UserOutput.java
@@ -5,4 +5,19 @@ import lombok.Data;
 @Data
 public class UserOutput {
 
+    private String username;
+    private String email;
+    private String phoneNumber;
+    private String role;
+    private boolean isEnabled;
+    private long transactionCount;
+
+    public UserOutput(String username, String email, String phoneNumber, String role, boolean isEnabled, long transactionCount) {
+        this.username = username;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.isEnabled = isEnabled;
+        this.transactionCount = transactionCount;
+    }
 }
