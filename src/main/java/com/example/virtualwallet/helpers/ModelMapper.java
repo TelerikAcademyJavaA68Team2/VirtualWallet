b07e429d364751ae639aa1e317b977b08b18509c
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-01 15:01:00 +0200

    cleaning ModelMapper

--- src/main/java/com/example/virtualwallet/helpers/ModelMapper.java
+++ src/main/java/com/example/virtualwallet/helpers/ModelMapper.java
@@ -1,22 +1,10 @@
 package com.example.virtualwallet.helpers;
 
-import com.example.virtualwallet.models.Dtos.UserOutput;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ModelMapper {
 
-    public UserOutput mapObjectToUserOutput(Object[] userObject) {
-        return new UserOutput((String) userObject[0],
-                (String) userObject[1],
-                (String) userObject[2],
-                (String) userObject[3],
-                (Boolean) userObject[4],
-                ((Number) userObject[5]).intValue());
-    }
 
-    public Page<UserOutput> mapObjectPageToUserOutputPage(Page<Object[]> usersObjectArray) {
-        return usersObjectArray.map(this::mapObjectToUserOutput);
-    }
+
 }
