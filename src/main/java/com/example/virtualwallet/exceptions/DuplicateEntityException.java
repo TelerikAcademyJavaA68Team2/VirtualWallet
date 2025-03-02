bd05029e6b47fe8cec8cbb1615aca8973f4aeee8
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-02 23:04:34 +0200

    updated DuplicateEntityException

--- src/main/java/com/example/virtualwallet/exceptions/DuplicateEntityException.java
+++ src/main/java/com/example/virtualwallet/exceptions/DuplicateEntityException.java
@@ -2,15 +2,15 @@ package com.example.virtualwallet.exceptions;
 
 public class DuplicateEntityException extends RuntimeException {
 
-    public DuplicateEntityException(String type) {
-        super(String.format("%s already exists.", type));
+    public DuplicateEntityException(String attribute) {
+        super(String.format("%s already exists.", attribute));
     }
 
-    public DuplicateEntityException(String type, String attribute) {
-        super(String.format("%s with %s already exists.", type, attribute));
+    public DuplicateEntityException(String attribute, String value) {
+        super(String.format("%s with %s already exists.", attribute, value));
     }
 
-    public DuplicateEntityException(String type, String number, String attribute) {
-        super(String.format("%s with %s %s already exists.", type, number, attribute));
+    public DuplicateEntityException(String entity, String attribute, String value) {
+        super(String.format("%s with %s: %s already exists.", entity, attribute, value));
     }
 }
