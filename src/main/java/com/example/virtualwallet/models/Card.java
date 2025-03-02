0bbbef459bb32d4108a2c965140503864745349c
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-02 22:21:09 +0200

    refactor

--- src/main/java/com/example/virtualwallet/models/Card.java
+++ src/main/java/com/example/virtualwallet/models/Card.java
@@ -5,12 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
 import java.time.YearMonth;
-import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
 
