ace9053b8322dd68cc78328e5f782f301c8faa37
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-03 20:49:07 +0200

    update: added update wallet method in WalletServiceImpl

--- src/main/java/com/example/virtualwallet/services/contracts/WalletService.java
+++ src/main/java/com/example/virtualwallet/services/contracts/WalletService.java
@@ -1,5 +1,6 @@
 package com.example.virtualwallet.services.contracts;
 
+import com.example.virtualwallet.models.User;
 import com.example.virtualwallet.models.Wallet;
 import com.example.virtualwallet.models.enums.Currency;
 
@@ -10,5 +11,7 @@ public interface WalletService {
     Wallet getWalletById(UUID id);
 
     Wallet getOrCreateWalletByUsernameAndCurrency(String userUsername, Currency currency);
+
+    void update(Wallet wallet, User user);
 
 }
