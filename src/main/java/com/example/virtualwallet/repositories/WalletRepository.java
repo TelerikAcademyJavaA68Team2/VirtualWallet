9efbe17018475af25a053caca2140586dc637fc7
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-03 11:05:36 +0200

    adding WalletRepository

---
+++ src/main/java/com/example/virtualwallet/repositories/WalletRepository.java
@@ -0,0 +1,20 @@
+package com.example.virtualwallet.repositories;
+
+import com.example.virtualwallet.models.Wallet;
+import com.example.virtualwallet.models.enums.Currency;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    @Query("SELECT w FROM Wallet w " +
+            "JOIN FETCH w.owner u " +
+            "WHERE u.username = :username AND w.currency = :currency")
+    Optional<Wallet> findByUsernameAndCurrency(@Param("username") String username, @Param("currency") Currency currency);
+}
