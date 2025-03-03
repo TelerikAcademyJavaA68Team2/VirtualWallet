055aa2d9e1046d0510bffd07acfb56394d2e7a45
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-03 20:53:25 +0200

    update: added repository annotation

--- src/main/java/com/example/virtualwallet/repositories/UserRepository.java
+++ src/main/java/com/example/virtualwallet/repositories/UserRepository.java
@@ -4,11 +4,13 @@ import com.example.virtualwallet.models.User;
 import com.example.virtualwallet.models.dtos.user.UserOutput;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
  /*   @Query("""
