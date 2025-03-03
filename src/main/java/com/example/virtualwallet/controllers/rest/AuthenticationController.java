b271bdd405571e50dbb6da68a96b7166342c5bd6
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-03 21:15:04 +0200

    update: added partial Swagger Documentation to AuthenticationController

--- src/main/java/com/example/virtualwallet/controllers/rest/AuthenticationController.java
+++ src/main/java/com/example/virtualwallet/controllers/rest/AuthenticationController.java
@@ -5,6 +5,9 @@ import com.example.virtualwallet.auth.emailVerification.EmailConfirmationService
 import com.example.virtualwallet.exceptions.EmailConfirmationException;
 import com.example.virtualwallet.models.dtos.auth.LoginUserInput;
 import com.example.virtualwallet.models.dtos.auth.RegisterUserInput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,16 +20,30 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "Authentication Management", description = "APIs for managing authentication and registration")
 public class AuthenticationController {
 
     private final AuthenticationService authService;
     private final EmailConfirmationService emailConfirmationService;
 
+    @Operation(summary = "Register a New User", description = "Register a new user account",
+            responses = {
+            @ApiResponse(description = "Success", responseCode = "200"),
+            @ApiResponse(description = "Conflict - Duplicate user", responseCode = "409"),
+            @ApiResponse(description = "Bad Request - Invalid user input", responseCode = "400")
+    })
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterUserInput request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
+    @Operation(summary = "User Login", description = "Authenticate an existing user",
+            responses = {
+            @ApiResponse(description = "Success", responseCode = "200"),
+            @ApiResponse(description = "Bad Request - User not found", responseCode = "400"),
+            @ApiResponse(description = "Unauthorized - Account blocked", responseCode = "401")
+    }
+    )
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginUserInput request) {
         return ResponseEntity.ok(authService.authenticate(request));
