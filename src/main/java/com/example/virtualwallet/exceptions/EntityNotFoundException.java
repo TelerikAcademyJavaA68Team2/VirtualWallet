10e87621ef8f30f692453ac9aa813bd77b3cb120
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-03 11:04:46 +0200

    updating EntityNotFoundException with 3param constructor

--- src/main/java/com/example/virtualwallet/exceptions/EntityNotFoundException.java
+++ src/main/java/com/example/virtualwallet/exceptions/EntityNotFoundException.java
@@ -10,4 +10,8 @@ public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String type, UUID id) {
         super(String.format("%s with id %s not found.", type, id));
     }
+
+    public EntityNotFoundException(String entity, String attribute, String value) {
+        super(String.format("%s with %s: %s not found.", entity, attribute, value));
+    }
 }
