8c4c1546300638d741cce8df3fa80b4a9e8c91ac
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-04 10:56:30 +0200

    delete empty lines

--- src/main/java/com/example/virtualwallet/repositories/UserRepository.java
+++ src/main/java/com/example/virtualwallet/repositories/UserRepository.java
@@ -13,37 +13,6 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
- /*   @Query("""
-            SELECT NEW com.example.virtualwallet.models.Dtos.UserOutput(
-                u.id,
-                u.username,
-                u.email,
-                u.phoneNumber,
-                u.role,
-                u.status,
-                CAST(COALESCE(SUM(w.balance), 0) AS BIGDECIMAL)
-            )
-            FROM User u
-            LEFT JOIN Wallet w ON u.id = w.owner.id
-            WHERE (:username IS NULL OR u.username = :username)
-              AND (:email IS NULL OR u.email = :email)
-              AND (:phoneNumber IS NULL OR u.phoneNumber LIKE CONCAT('%', :phoneNumber, '%'))
-              AND (:role IS NULL OR u.role = :role)
-              AND (:accountStatus IS NULL OR (u.status = :acountStatus))
-            GROUP BY u.id, u.username, u.email, u.phoneNumber, u.role, u.status
-            HAVING (:minTotalBalance IS NULL OR COALESCE(SUM(w.balance), 0) >= :minTotalBalance)
-               AND (:maxTotalBalance IS NULL OR COALESCE(SUM(w.balance), 0) <= :maxTotalBalance)
-            """)
-    Page<UserOutput> findUsersWithTotalBalance(
-            @Param("username") String username,
-            @Param("email") String email,
-            @Param("phoneNumber") String phoneNumber,
-            @Param("role") Role role,
-            @Param("accountStatus") String accountStatus,
-            @Param("minTotalBalance") BigDecimal minTotalBalance,
-            @Param("maxTotalBalance") BigDecimal maxTotalBalance,
-            Pageable pageable);
-*/
 
     @Query("""
     SELECT NEW com.example.virtualwallet.models.dtos.user.UserOutput(
