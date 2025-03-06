6a25c8e3593e7b3074dfd6e43a30f156fd508844
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-06 23:18:07 +0200

    updated UserOutput dto

--- src/main/java/com/example/virtualwallet/models/dtos/user/UserOutput.java
+++ src/main/java/com/example/virtualwallet/models/dtos/user/UserOutput.java
@@ -5,17 +5,18 @@ import com.example.virtualwallet.models.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 public class UserOutput {
     private UUID id;
+    private String photo;
     private String username;
     private String email;
     private String phoneNumber;
     private Role role;
     private AccountStatus accountStatus;
-    private BigDecimal totalBalance;
+    private LocalDateTime createdAt;
 }
