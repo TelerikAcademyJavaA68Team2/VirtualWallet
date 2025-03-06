23c1d766c516dd4240b84d35aa274acbbf47264e
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-06 23:15:33 +0200

    added /admin/users with filteroptions

--- src/main/java/com/example/virtualwallet/controllers/rest/AdminRestController.java
+++ src/main/java/com/example/virtualwallet/controllers/rest/AdminRestController.java
@@ -1,22 +1,14 @@
 package com.example.virtualwallet.controllers.rest;
 
-import com.example.virtualwallet.models.dtos.user.UserOutput;
 import com.example.virtualwallet.models.fillterOptions.UserFilterOptions;
 import com.example.virtualwallet.services.contracts.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -26,28 +18,13 @@ public class AdminRestController {
 
     private final UserService userService;
 
-    @Operation(
-            summary = "Retrieve all users",
-            description = "Fetch a list of users",
-            responses = {
-                    @ApiResponse(description = "Success", responseCode = "200")
-            }
-    )
     @GetMapping("/users")
-    public ResponseEntity<?> getAllUsersNoFilters() {
-        return ResponseEntity.ok(userService.getAllUsers());
-    }
-
-
-    @GetMapping("/usersWithFilters")
-    public ResponseEntity<?> getAllUsers2(
+    public ResponseEntity<?> filterUsers(
             @RequestParam(required = false) String username,
             @RequestParam(required = false) String email,
             @RequestParam(required = false) String phoneNumber,
             @RequestParam(required = false) String role,
             @RequestParam(required = false) String accountStatus,
-            @RequestParam(required = false) BigDecimal minTotalBalance,
-            @RequestParam(required = false) BigDecimal maxTotalBalance,
             @RequestParam(defaultValue = "username") String sortBy,
             @RequestParam(defaultValue = "asc") String sortOrder,
             @RequestParam(defaultValue = "0") int page,
@@ -56,26 +33,20 @@ public class AdminRestController {
             return ResponseEntity.badRequest().body("Invalid page or size parameters.");
         }
 
-        Pageable pageable = PageRequest.of(page, size);
         UserFilterOptions userFilterOptions = new UserFilterOptions(
                 username,
                 email,
                 phoneNumber,
                 role,
                 accountStatus,
-                minTotalBalance,
-                maxTotalBalance,
                 sortBy,
-                sortOrder
+                sortOrder,
+                page,
+                size
         );
 
-        Page<UserOutput> userPage = userService.filterUsers(userFilterOptions, pageable);
+        return ResponseEntity.ok(userService.filterUsers(userFilterOptions));
 
-        if (userPage.hasContent()) {
-            return ResponseEntity.ok(userPage);
-        } else {
-            return ResponseEntity.noContent().build();
-        }
     }
 
 }
