f35fa89020a69754462edc9c27765d7b1406ebcd
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-06 19:47:12 +0200

    feat: added CardServiceImplTests for CardServiceImpl with 100% coverage

--- src/test/java/com/example/virtualwallet/Helpers.java
+++ src/test/java/com/example/virtualwallet/Helpers.java
@@ -1,14 +1,26 @@
-/*
 package com.example.virtualwallet;
 
-import com.example.virtualwallet.models.user.dtos.UserOutput;
-import com.example.virtualwallet.models.User;
+import com.example.virtualwallet.models.Card;
+import com.example.virtualwallet.models.Transfer;
+import com.example.virtualwallet.models.Wallet;
+import com.example.virtualwallet.models.dtos.CardEdit;
+import com.example.virtualwallet.models.dtos.CardInput;
+import com.example.virtualwallet.models.dtos.CardOutput;
+import com.example.virtualwallet.models.dtos.CardOutputForList;
+import com.example.virtualwallet.models.dtos.user.UserOutput;
+import com.example.virtualwallet.models.enums.AccountStatus;
 import com.example.virtualwallet.models.enums.Currency;
+import com.example.virtualwallet.models.enums.TransactionStatus;
+import com.example.virtualwallet.models.User;
 import com.example.virtualwallet.models.enums.Role;
-import com.example.virtualwallet.models.fillterOptions.UserFilterOptions;
 import org.springframework.data.domain.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class Helpers {
@@ -17,6 +29,10 @@ public class Helpers {
     public static final String MOCK_ADMIN_FIRST_NAME = "MockFirstNameAdmin";
     public static final String MOCK_USER_LAST_NAME = "MockLastNameUser";
     public static final String MOCK_ADMIN_LAST_NAME = "MockLastNameAdmin";
+    public static final String MOCK_CARD_CARDHOLDER = "Mock Holder";
+    public static final String MOCK_CARD_CARDHOLDER_EDIT = "Mock Holder Edited";
+    public static final String MOCK_CARD_CARD_NUMBER = "1234567891234567";
+    public static final String MOCK_CARD_CARD_NUMBER_ANOTHER = "9234567891234567";
     public static final String MOCK_USER_USERNAME = "MockUsername";
     public static final String MOCK_ADMIN_USERNAME = "MockAdminName";
     public static final String MOCK_PASSWORD = "MockPassword";
@@ -25,9 +41,25 @@ public class Helpers {
     public static final String MOCK_ADMIN_EMAIL = "mock@admin.com";
     public static final String MOCK_ACCOUNT_STATUS = "ACTIVE";
     public static final String MOCK_USER_ORDER_BY = "username";
+    public static final String MOCK_CVV = "123";
+    public static final YearMonth MOCK_YEAR_MONTH_EXPIRATION_DATE = YearMonth.of(2025, 7);
+    public static final String MOCK_STRING_EXPIRATION_DATE = "07/25";
 
+    public static User createMockUserWithCardsAndWallets() {
+        User mockUser = createMockUserWithoutCardsAndWallets();
 
-    public static User createMockUser() {
+        Wallet mockWallet = createMockWallet(mockUser);
+        Card mockCard = createMockCard(mockUser);
+        Transfer mockTransfer = createMockTransfer(mockCard, mockWallet);
+
+        mockCard.getTransfers().add(mockTransfer);
+        mockUser.setWallets(Set.of(mockWallet));
+        mockUser.setCards(Set.of(mockCard));
+
+        return mockUser;
+    }
+
+    public static User createMockUserWithoutCardsAndWallets() {
         User mockUser = new User();
         mockUser.setId(UUID.randomUUID());
         mockUser.setFirstName(MOCK_USER_FIRST_NAME);
@@ -36,46 +68,132 @@ public class Helpers {
         mockUser.setUsername(MOCK_USER_USERNAME);
         mockUser.setPassword(MOCK_PASSWORD);
         mockUser.setRole(Role.USER);
-        mockUser.setCurrency(Currency.BGN);
+        mockUser.setPhoto("/static/images/default-profile-pic.png");
+        mockUser.setStatus(AccountStatus.ACTIVE);
+        mockUser.setRole(Role.USER);
+        mockUser.setCreatedAt(LocalDateTime.now());
         return mockUser;
     }
 
-    public static UserOutput createMockUserOutput() {
-        return new UserOutput(MOCK_USER_USERNAME,
-                MOCK_USER_EMAIL,
-                MOCK_PHONE_NUMBER,
-                Role.USER.name(),
-                true,
-                1L
-        );
+    public static Card createMockCard(User owner) {
+        Card mockCard = new Card();
+        mockCard.setId(UUID.randomUUID());
+        mockCard.setCardHolder(MOCK_CARD_CARDHOLDER);
+        mockCard.setCardNumber(MOCK_CARD_CARD_NUMBER);
+        mockCard.setCvv(MOCK_CVV);
+        mockCard.setExpirationDate(MOCK_YEAR_MONTH_EXPIRATION_DATE);
+        mockCard.setCreatedAt(LocalDateTime.now());
+        mockCard.setOwner(owner);
+        mockCard.setTransfers(new HashSet<>());
+
+        return mockCard;
+    }
+
+    public static Card createMockDeletedCard(User owner) {
+        Card mockCard = new Card();
+        mockCard.setId(UUID.randomUUID());
+        mockCard.setCardHolder(MOCK_CARD_CARDHOLDER);
+        mockCard.setCardNumber(MOCK_CARD_CARD_NUMBER);
+        mockCard.setCvv(MOCK_CVV);
+        mockCard.setExpirationDate(MOCK_YEAR_MONTH_EXPIRATION_DATE);
+        mockCard.setCreatedAt(LocalDateTime.now());
+        mockCard.setOwner(owner);
+        mockCard.setTransfers(new HashSet<>());
+        mockCard.markAsDeleted();
+
+        return mockCard;
+    }
+
+    public static CardOutput createMockCardOutput() {
+        CardOutput mockCardOutput = new CardOutput();
+        mockCardOutput.setCardId(UUID.randomUUID());
+        mockCardOutput.setCardHolder(MOCK_CARD_CARDHOLDER);
+        mockCardOutput.setCardNumber(MOCK_CARD_CARD_NUMBER);
+        mockCardOutput.setCvv(MOCK_CVV);
+        mockCardOutput.setExpirationDate(MOCK_YEAR_MONTH_EXPIRATION_DATE);
+        return mockCardOutput;
+    }
+
+    public static CardOutputForList createMockCardOutputForList() {
+        CardOutputForList mockCardOutputForList = new CardOutputForList();
+        mockCardOutputForList.setCardId(UUID.randomUUID());
+        mockCardOutputForList.setCardNumber(MOCK_CARD_CARD_NUMBER);
+        mockCardOutputForList.setExpirationDate(MOCK_YEAR_MONTH_EXPIRATION_DATE);
+        return mockCardOutputForList;
+    }
+
+    public static CardInput createMockCardInput() {
+        CardInput cardInput = new CardInput();
+        cardInput.setCardHolder(MOCK_CARD_CARDHOLDER);
+        cardInput.setCardNumber(MOCK_CARD_CARD_NUMBER);
+        cardInput.setCvv(MOCK_CVV);
+        cardInput.setExpirationDate(MOCK_STRING_EXPIRATION_DATE);
+        return cardInput;
+    }
+
+    public static CardEdit createMockCardEdit() {
+        CardEdit cardEdit = new CardEdit();
+        cardEdit.setCardHolder(MOCK_CARD_CARDHOLDER_EDIT);
+        cardEdit.setCardNumber(MOCK_CARD_CARD_NUMBER);
+        cardEdit.setCvv(MOCK_CVV);
+        cardEdit.setExpirationDate(MOCK_STRING_EXPIRATION_DATE);
+        return cardEdit;
+    }
+
+    public static Wallet createMockWallet(User owner) {
+        Wallet mockWallet = new Wallet();
+        mockWallet.setId(UUID.randomUUID());
+        mockWallet.setCurrency(Currency.USD);
+        mockWallet.setBalance(BigDecimal.valueOf(100.00));
+        mockWallet.setCreatedAt(LocalDateTime.now());
+        mockWallet.setOwner(owner);
+
+        return mockWallet;
+    }
+
+    public static Transfer createMockTransfer(Card card, Wallet wallet) {
+        Transfer mockTransfer = new Transfer();
+        mockTransfer.setId(UUID.randomUUID());
+        mockTransfer.setAmount(BigDecimal.valueOf(50.00));
+        mockTransfer.setStatus(TransactionStatus.APPROVED);
+        mockTransfer.setDate(LocalDateTime.now());
+        mockTransfer.setCard(card);
+        mockTransfer.setWallet(wallet);
+
+        return mockTransfer;
     }
 
     public static User createMockAdmin() {
-        User mockUser = new User();
-        mockUser.setId(UUID.randomUUID());
-        mockUser.setFirstName(MOCK_ADMIN_FIRST_NAME);
-        mockUser.setLastName(MOCK_ADMIN_LAST_NAME);
-        mockUser.setUsername(MOCK_ADMIN_USERNAME);
-        mockUser.setPassword(MOCK_PASSWORD);
-        mockUser.setEmail(MOCK_ADMIN_EMAIL);
-        mockUser.setPhoneNumber(MOCK_PHONE_NUMBER);
-        mockUser.setRole(Role.ADMIN);
-        mockUser.setCurrency(Currency.BGN);
-        return mockUser;
+        User mockAdmin = createMockUserWithCardsAndWallets();
+        mockAdmin.setRole(Role.ADMIN);
+        mockAdmin.setUsername(MOCK_ADMIN_USERNAME);
+        mockAdmin.setEmail(MOCK_ADMIN_EMAIL);
+        return mockAdmin;
     }
 
-    public static UserFilterOptions createMockUserFilterOptions() {
-        return new UserFilterOptions(
-                MOCK_PHONE_NUMBER,
+    public static UserOutput createMockUserOutput() {
+        return new UserOutput(UUID.randomUUID(),
                 MOCK_USER_USERNAME,
                 MOCK_USER_EMAIL,
-                Role.USER.name(),
-                MOCK_ACCOUNT_STATUS,
-                0,
-                10,
-                MOCK_USER_ORDER_BY
+                MOCK_PHONE_NUMBER,
+                Role.USER,
+                AccountStatus.ACTIVE,
+                BigDecimal.valueOf(50.00)
         );
     }
+
+//    public static UserFilterOptions createMockUserFilterOptions() {
+//        return new UserFilterOptions(
+//                MOCK_PHONE_NUMBER,
+//                MOCK_USER_USERNAME,
+//                MOCK_USER_EMAIL,
+//                Role.USER.name(),
+//                MOCK_ACCOUNT_STATUS,
+//                0,
+//                10,
+//                MOCK_USER_ORDER_BY
+//        );
+//    }
 
     public static Pageable createMockPageable() {
         return PageRequest.of(0, 20, Sort.unsorted());
@@ -91,4 +209,4 @@ public class Helpers {
                 5L
         }));
     }
-}*/
+}
--- src/test/java/com/example/virtualwallet/services/CardServiceImplTests.java
+++ src/test/java/com/example/virtualwallet/services/CardServiceImplTests.java
@@ -1,13 +1,393 @@
 package com.example.virtualwallet.services;
 
+import com.example.virtualwallet.exceptions.DuplicateEntityException;
+import com.example.virtualwallet.exceptions.EntityNotFoundException;
+import com.example.virtualwallet.exceptions.UnauthorizedAccessException;
+import com.example.virtualwallet.helpers.ModelMapper;
+import com.example.virtualwallet.models.Card;
+import com.example.virtualwallet.models.User;
+import com.example.virtualwallet.models.dtos.CardEdit;
+import com.example.virtualwallet.models.dtos.CardInput;
+import com.example.virtualwallet.models.dtos.CardOutput;
+import com.example.virtualwallet.models.dtos.CardOutputForList;
+import com.example.virtualwallet.repositories.CardRepository;
+import com.example.virtualwallet.services.contracts.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.*;
+
+import static com.example.virtualwallet.Helpers.*;
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
+@TestPropertySource(locations = "classpath:messages.properties")
 public class CardServiceImplTests {
+
+    public static final String REGISTERED_TO_ANOTHER_USER = "This card is already registered to another user!";
+
+    @Mock
+    private Environment env;
+
+    @Mock
+    private CardRepository cardRepository;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ModelMapper modelMapper;
 
     @InjectMocks
     CardServiceImpl cardService;
+
+
+    @Test
+    public void getCardById_ShouldReturnCard_When_ValidArgs() {
+
+        User user = createMockUserWithoutCardsAndWallets();
+        Card mockCard = createMockCard(user);
+        when(cardRepository.findById(any())).thenReturn(Optional.of(mockCard));
+
+        Card result = cardService.getCardById(mockCard.getId());
+
+        assertNotNull(result);
+        Assertions.assertEquals(mockCard, result);
+        verify(cardRepository, times(1)).findById(mockCard.getId());
+
+    }
+
+    @Test
+    public void getCardOutputById_ShouldReturnCardOutput_When_ValidArgs() {
+
+        Card mockCard = createMockCard(createMockUserWithCardsAndWallets());
+        CardOutput mockCardOutput = createMockCardOutput();
+        mockCardOutput.setCardId(mockCard.getId());
+
+        when(cardRepository.findById(eq(mockCard.getId()))).thenReturn(Optional.of(mockCard));
+        when(modelMapper.cardOutputFromCard(mockCard)).thenReturn(mockCardOutput);
+
+        // Act
+        CardOutput result = cardService.getCardOutputById(mockCard.getId());
+
+        assertNotNull(result);
+        Assertions.assertEquals(mockCardOutput, result);
+        verify(cardRepository, times(1)).findById(mockCardOutput.getCardId());
+        verify(modelMapper, times(1)).cardOutputFromCard(mockCard);
+
+    }
+
+    @Test
+    public void getById_ShouldThrow_Exception_When_CardNotFound() {
+        when(cardRepository.findById(any())).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> cardService.getCardById(UUID.randomUUID()));
+
+    }
+
+    @Test
+    public void getAllCards_Should_ReturnEmptyList() {
+        // Arrange
+        User user = createMockUserWithoutCardsAndWallets();
+        user.setCards(new HashSet<>());
+        when(cardRepository.getAllByOwner_Id(user.getId()))
+                .thenReturn(new ArrayList<>());
+
+        // Act
+        List<Card> cards = cardService.getAllCardsByUser(user.getId());
+
+        // Assert
+        verify(cardRepository, Mockito.times(1))
+                .getAllByOwner_Id(user.getId());
+
+        Assertions.assertEquals(user.getCards().isEmpty(), cards.isEmpty());
+    }
+
+    @Test
+    public void getAllCards_Should_CallRepository() {
+        // Act
+        cardService.getAllCardsByUser(new UUID(1, 1));
+
+        // Assert
+        verify(cardRepository, Mockito.times(1))
+                .getAllByOwner_Id(new UUID(1, 1));
+    }
+
+    @Test
+    public void getAllCards_Should_ReturnUserCards() {
+        // Arrange
+        User user = createMockUserWithCardsAndWallets();
+        when(cardRepository.getAllByOwner_Id(user.getId()))
+                .thenReturn(user.getCards().stream().toList());
+
+        // Act
+        List<Card> cards = cardService.getAllCardsByUser(user.getId());
+
+        // Assert
+        verify(cardRepository, Mockito.times(1))
+                .getAllByOwner_Id(user.getId());
+
+        Assertions.assertEquals(user.getCards().stream().toList(), cards);
+    }
+
+    @Test
+    public void getTotalNumberOfCardsByOwnerId_Should_ReturnCorrectCount() {
+        // Arrange
+        UUID userId = UUID.randomUUID();
+        int expectedCardCount = 5;
+
+        when(cardRepository.getTotalNumberOfCardsByOwner_Id(userId)).thenReturn(expectedCardCount);
+
+        // Act
+        int actualCardCount = cardService.getTotalNumberOfCardsByOwner_Id(userId);
+
+        // Assert
+        verify(cardRepository, times(1)).getTotalNumberOfCardsByOwner_Id(userId);
+        assertEquals(expectedCardCount, actualCardCount);
+    }
+
+    @Test
+    public void getAllCardsOutputForList_Should_ReturnUserCardsOutPutForList() {
+        // Arrange
+        User user = createMockUserWithoutCardsAndWallets();
+        Card mockCard = createMockCard(user);
+        user.setCards(new HashSet<>(List.of(mockCard)));
+
+        CardOutputForList mockCardOutputForList = createMockCardOutputForList();
+        mockCardOutputForList.setCardId(mockCard.getId());
+
+        when(cardRepository.getAllByOwner_Id(user.getId()))
+                .thenReturn(user.getCards().stream().toList());
+        when(modelMapper.displayForListCardOutputFromCreditCard(mockCard)).thenReturn(mockCardOutputForList);
+
+        List<CardOutputForList> expectedCardsOutputForList = List.of(mockCardOutputForList);
+
+        // Act
+        List<CardOutputForList> cards = cardService.getAllCardsOutputForListByUser(user.getId());
+
+        // Assert
+        assertNotNull(cards);
+        assertEquals(1, cards.size());
+        Assertions.assertEquals(expectedCardsOutputForList, cards);
+        assertEquals(mockCardOutputForList, cards.get(0));
+
+        verify(cardRepository, Mockito.times(1))
+                .getAllByOwner_Id(user.getId());
+
+        verify(modelMapper, times(1)).displayForListCardOutputFromCreditCard(mockCard);
+    }
+
+    @Test
+    public void addCard_Should_AddCard_WhenValidArgsAndNoCardExists() {
+        CardInput cardInput = createMockCardInput();
+        User user = createMockUserWithoutCardsAndWallets();
+        Card card = createMockCard(user);
+        CardOutput cardOutput = createMockCardOutput();
+        cardOutput.setCardId(card.getId());
+
+        when(userService.getAuthenticatedUser())
+                .thenReturn(user);
+        when(cardRepository.getCardByCardNumber(card.getCardNumber())).thenReturn(null);
+        when(modelMapper.createCardFromCardInput(cardInput, user)).thenReturn(card);
+        when(cardRepository.save(card)).thenReturn(card);
+        when(modelMapper.cardOutputFromCard(card)).thenReturn(cardOutput);
+
+        CardOutput cardOutputResult = cardService.addCard(cardInput);
+
+        verify(cardRepository, Mockito.times(1)).save(card);
+        verify(cardRepository, Mockito.times(1)).getCardByCardNumber(card.getCardNumber());
+
+        assertEquals(cardOutputResult, cardOutput);
+        assertNotNull(cardOutputResult);
+    }
+
+    @Test
+    public void addCard_Should_MakeCardActive_WhenCardIsSoftDeleted() {
+        CardInput cardInput = createMockCardInput();
+        User user = createMockUserWithoutCardsAndWallets();
+        Card card = createMockCard(user);
+        card.markAsDeleted();
+        CardOutput cardOutput = createMockCardOutput();
+        cardOutput.setCardId(card.getId());
+
+        when(userService.getAuthenticatedUser())
+                .thenReturn(user);
+        when(cardRepository.getCardByCardNumber(card.getCardNumber())).thenReturn(card);
+        when(modelMapper.modifySoftDeletedCardFromCardInput(card, cardInput)).thenReturn(card);
+        when(cardRepository.save(card)).thenReturn(card);
+        when(modelMapper.cardOutputFromCard(card)).thenReturn(cardOutput);
+
+        CardOutput cardOutputResult = cardService.addCard(cardInput);
+
+        verify(cardRepository, Mockito.times(1)).save(card);
+        verify(cardRepository, Mockito.times(1)).getCardByCardNumber(card.getCardNumber());
+
+        assertEquals(cardOutputResult, cardOutput);
+        assertFalse(card.isDeleted());
+        assertNull(card.getDeletedAt());
+        assertEquals(card.getOwner(), user);
+        assertNotNull(cardOutputResult);
+    }
+
+    @Test
+    public void addCard_Should_ThrowDuplicateEntityException_When_DifferentUserTriesToAddCardWithSameNumber() {
+        CardInput cardInput = createMockCardInput();
+        User user = createMockUserWithoutCardsAndWallets();
+        User anotherUser = createMockUserWithoutCardsAndWallets();
+        Card card = createMockCard(anotherUser);
+
+        when(userService.getAuthenticatedUser()).thenReturn(user);
+        when(cardRepository.getCardByCardNumber(card.getCardNumber())).thenReturn(card);
+        when(env.getProperty("error.registeredAnotherUser"))
+                .thenReturn(REGISTERED_TO_ANOTHER_USER);
+
+        DuplicateEntityException duplicateEntityException = Assertions.assertThrows(DuplicateEntityException.class,
+                () -> cardService.addCard(cardInput));
+
+        verify(cardRepository, never()).save(any(Card.class));
+
+        assertEquals(REGISTERED_TO_ANOTHER_USER, duplicateEntityException.getMessage());
+
+    }
+
+    @Test
+    public void addCard_Should_ThrowDuplicateEntityException_When_TheSameUserTriesToAddExistingCard() {
+        CardInput cardInput = createMockCardInput();
+        User user = createMockUserWithoutCardsAndWallets();
+        Card card = createMockCard(user);
+
+        when(userService.getAuthenticatedUser()).thenReturn(user);
+        when(cardRepository.getCardByCardNumber(card.getCardNumber())).thenReturn(card);
+
+        DuplicateEntityException duplicateEntityException = Assertions.assertThrows(DuplicateEntityException.class,
+                () -> cardService.addCard(cardInput));
+
+        verify(cardRepository, never()).save(any(Card.class));
+
+        assertEquals(format("Card with number %s already exists.", card.getCardNumber()), duplicateEntityException.getMessage());
+
+    }
+
+    @Test
+    public void updateCard_Should_UpdateCard_When_UserIsOwner() {
+        CardEdit cardEdit = createMockCardEdit();
+        User user = createMockUserWithoutCardsAndWallets();
+        Card card = createMockCard(user);
+        Card cardUpdated = createMockCard(user);
+        cardUpdated.setCardHolder(cardEdit.getCardHolder());
+
+        CardOutput cardOutput = createMockCardOutput();
+        cardOutput.setCardHolder(cardEdit.getCardHolder());
+
+        when(userService.getAuthenticatedUser()).thenReturn(user);
+        when(cardRepository.findById(any())).thenReturn(Optional.of(card));
+        when(modelMapper.updateCardFromCardInput(cardEdit, card)).thenReturn(cardUpdated);
+        when(cardRepository.save(cardUpdated)).thenReturn(cardUpdated);
+        when(modelMapper.cardOutputFromCard(cardUpdated)).thenReturn(cardOutput);
+
+        CardOutput result = cardService.updateCard(cardEdit, card.getId());
+
+        verify(cardRepository, Mockito.times(1)).findById(card.getId());
+        verify(cardRepository, Mockito.times(1)).save(cardUpdated);
+        assertEquals(result, cardOutput);
+
+    }
+
+    @Test
+    public void updateCard_Should_ThrowUnauthorizedAccessException_When_IsNotOwner() {
+
+        CardEdit cardEdit = createMockCardEdit();
+        cardEdit.setCardNumber(MOCK_CARD_CARD_NUMBER_ANOTHER);
+
+        User user = createMockUserWithoutCardsAndWallets();
+        User anotherUser = createMockAdmin();
+
+        Card card = createMockCard(user);
+        card.setCardNumber(MOCK_CARD_CARD_NUMBER_ANOTHER);
+
+        Card anotherCard = createMockCard(anotherUser);
+        anotherCard.setCardNumber(MOCK_CARD_CARD_NUMBER_ANOTHER);
+
+        when(userService.getAuthenticatedUser()).thenReturn(user);
+        when(cardRepository.findById(any())).thenReturn(Optional.of(anotherCard));
+
+        UnauthorizedAccessException unauthorizedAccessException = Assertions.assertThrows(UnauthorizedAccessException.class,
+                () -> cardService.updateCard(cardEdit, card.getId()));
+
+        verify(cardRepository, never()).save(any(Card.class));
+        assertEquals("Only the card's owner can modify cards!", unauthorizedAccessException.getMessage());
+
+    }
+
+    @Test
+    public void updateCard_Should_DuplicateEntityException_When_CardNumberAlreadyExists() {
+
+        CardEdit cardEdit = createMockCardEdit();
+        cardEdit.setCardNumber(MOCK_CARD_CARD_NUMBER_ANOTHER);
+
+        User user = createMockUserWithoutCardsAndWallets();
+
+        Card card = createMockCard(user);
+
+        Card anotherCard = createMockCard(user);
+        anotherCard.setCardNumber(MOCK_CARD_CARD_NUMBER_ANOTHER);
+
+        when(userService.getAuthenticatedUser()).thenReturn(user);
+        when(cardRepository.findById(any())).thenReturn(Optional.of(card));
+        when(cardRepository.getCardByCardNumber(cardEdit.getCardNumber())).thenReturn(anotherCard);
+
+        Assertions.assertThrows(DuplicateEntityException.class,
+                () -> cardService.updateCard(cardEdit, card.getId()));
+
+        verify(cardRepository, never()).save(any(Card.class));
+
+    }
+
+
+    @Test
+    public void deleteCard_Should_SoftDeleteCardById() {
+        User user = createMockUserWithoutCardsAndWallets();
+        Card card = createMockCard(user);
+
+
+        when(userService.getAuthenticatedUser()).thenReturn(user);
+        when(cardRepository.findById(any())).thenReturn(Optional.of(card));
+        when(cardRepository.save(any())).thenReturn(card);
+
+        cardService.softDeleteCard(card.getId());
+
+        assertSame(card.getOwner(), user);
+        verify(cardRepository, Mockito.times(1)).save(card);
+        assertTrue(card.isDeleted());
+    }
+
+    @Test
+    public void deleteCard_ShouldThrow_UnauthorizedAccessException_When_UserIsNotOwner() {
+        User user = createMockUserWithoutCardsAndWallets();
+        User anotherUser = createMockUserWithoutCardsAndWallets();
+        Card card = createMockCard(anotherUser);
+
+        when(userService.getAuthenticatedUser()).thenReturn(user);
+        when(cardRepository.findById(any())).thenReturn(Optional.of(card));
+
+        Assertions.assertThrows(UnauthorizedAccessException.class,
+                () -> cardService.softDeleteCard(card.getId()));
+
+        assertNotSame(card.getOwner(), user);
+        verify(cardRepository, never()).save(any(Card.class));
+        assertFalse(card.isDeleted());
+    }
+
 
 }
