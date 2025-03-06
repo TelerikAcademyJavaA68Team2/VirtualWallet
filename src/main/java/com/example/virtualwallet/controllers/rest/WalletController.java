2630854f22e25fca810b2f88285eae86d18ee895
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-07 00:58:29 +0200

    refactor

--- src/main/java/com/example/virtualwallet/controllers/rest/WalletController.java
+++ src/main/java/com/example/virtualwallet/controllers/rest/WalletController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/wallets")
+@RequestMapping("/api/wallets")
 public class WalletController {
 
 
