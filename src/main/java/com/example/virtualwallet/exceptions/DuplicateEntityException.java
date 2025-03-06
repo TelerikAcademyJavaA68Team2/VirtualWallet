1598151e46bfad1a9896344229a9cc395b691815
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-06 18:08:58 +0200

    refactor: DuplicateEntityException

--- src/main/java/com/example/virtualwallet/exceptions/DuplicateEntityException.java
+++ src/main/java/com/example/virtualwallet/exceptions/DuplicateEntityException.java
@@ -11,6 +11,6 @@ public class DuplicateEntityException extends RuntimeException {
     }
 
     public DuplicateEntityException(String entity, String attribute, String value) {
-        super(String.format("%s with %s: %s already exists.", entity, attribute, value));
+        super(String.format("%s with %s %s already exists.", entity, attribute, value));
     }
 }
