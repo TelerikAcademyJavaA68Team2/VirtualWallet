779f7e5b06a6f3b8ef24a5b364a259df50ab5b4f
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-06 18:14:52 +0200

    update: updated added date in TransferOutput

--- src/main/java/com/example/virtualwallet/models/dtos/TransferOutput.java
+++ src/main/java/com/example/virtualwallet/models/dtos/TransferOutput.java
@@ -3,12 +3,15 @@ package com.example.virtualwallet.models.dtos;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 public class TransferOutput {
 
     private UUID transferId;
+
+    private LocalDateTime date;
 
     private String status;
 
