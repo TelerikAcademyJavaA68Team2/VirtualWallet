474f052eefa0a94f3ad65b88fbb7eeae6fde8e82
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-07 13:09:03 +0200

    added new endpoint in WalletController

--- src/main/java/com/example/virtualwallet/controllers/rest/WalletController.java
+++ src/main/java/com/example/virtualwallet/controllers/rest/WalletController.java
@@ -1,13 +1,24 @@
 package com.example.virtualwallet.controllers.rest;
 
+import com.example.virtualwallet.services.contracts.WalletService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/wallets")
 public class WalletController {
 
+    private final WalletService walletService;
 
+    @GetMapping("{walletId}")
+    public ResponseEntity<?> getWalletHistory(@PathVariable UUID walletId,
+                                              @RequestParam(defaultValue = "0") int page,
+                                              @RequestParam(defaultValue = "10") int size) {
+        return new ResponseEntity<>(walletService.getWalletHistoryPageById(walletId, page, size), HttpStatus.OK);
+    }
 }
