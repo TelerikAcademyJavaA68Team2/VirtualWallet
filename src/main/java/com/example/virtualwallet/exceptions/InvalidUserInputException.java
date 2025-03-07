15006a6620b1c26aec08db56c4bb67707c15be33
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-08 00:46:30 +0200

    update: added InvalidUserInputException formatting for date

--- src/main/java/com/example/virtualwallet/exceptions/InvalidUserInputException.java
+++ src/main/java/com/example/virtualwallet/exceptions/InvalidUserInputException.java
@@ -1,7 +1,13 @@
 package com.example.virtualwallet.exceptions;
 
+import java.time.format.DateTimeParseException;
+
 public class InvalidUserInputException extends RuntimeException {
     public InvalidUserInputException(String message) {
+        super(message);
+    }
+
+    public InvalidUserInputException(String message, DateTimeParseException ex) {
         super(message);
     }
 }
