b5ad023b2e4e8f1f6d2bdcb7b0ced42da8b19001
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-09 16:54:36 +0200

    refactor

--- src/main/java/com/example/virtualwallet/services/contracts/TransactionService.java
+++ src/main/java/com/example/virtualwallet/services/contracts/TransactionService.java
@@ -1,8 +1,8 @@
 package com.example.virtualwallet.services.contracts;
 
 import com.example.virtualwallet.models.Transaction;
-import com.example.virtualwallet.models.dtos.transfer.TransactionInput;
-import com.example.virtualwallet.models.dtos.transfer.TransactionOutput;
+import com.example.virtualwallet.models.dtos.transactions.TransactionInput;
+import com.example.virtualwallet.models.dtos.transactions.TransactionOutput;
 import com.example.virtualwallet.models.fillterOptions.TransactionFilterOptions;
 
 import java.util.List;
