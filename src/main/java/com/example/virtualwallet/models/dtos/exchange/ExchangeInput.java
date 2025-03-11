7020e0b38874fabbfba2d03bf5739655c9945ce8
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-11 12:07:51 +0200

    update: moved message to messages.properties

--- src/main/java/com/example/virtualwallet/models/dtos/exchange/ExchangeInput.java
+++ src/main/java/com/example/virtualwallet/models/dtos/exchange/ExchangeInput.java
@@ -3,18 +3,20 @@ package com.example.virtualwallet.models.dtos.exchange;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
+import org.springframework.context.annotation.PropertySource;
 
 import java.math.BigDecimal;
 
 @Data
+@PropertySource("classpath:messages.properties")
 public class ExchangeInput {
 
-    @NotBlank(message = "From currency cant be blank!")
+    @NotBlank(message = "{error.fromCurrencyBlank}")
     private String fromCurrency;
 
-    @NotBlank(message = "To currency cant be blank!")
+    @NotBlank(message = "{error.toCurrencyBlank}")
     private String toCurrency;
 
-    @Positive(message = "The amount must be positive!")
+    @Positive(message = "{error.positiveNumber}")
     private BigDecimal amount;
 }
