e9bd0b2b21dbc07cdc4c97e649c4d38139603b43
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-11 03:49:54 +0200

    updated the dummy data and create scripts

--- db/create_schema.sql
+++ db/create_schema.sql
@@ -5,17 +5,17 @@ USE virtual_wallet;
 CREATE TABLE user
 (
     id           UUID PRIMARY KEY,
-    created_at   DATETIME(6),
-    deleted_at   DATETIME(6),
-    email        VARCHAR(255)                                     NOT NULL UNIQUE,
     first_name   VARCHAR(255)                                     NOT NULL,
     last_name    VARCHAR(255)                                     NOT NULL,
+    username     VARCHAR(255)                                     NOT NULL UNIQUE,
+    email        VARCHAR(255)                                     NOT NULL UNIQUE,
     password     VARCHAR(255)                                     NOT NULL,
     phone_number VARCHAR(255)                                     NOT NULL UNIQUE,
-    photo        VARCHAR(255),
+    photo        VARCHAR(255)                                     NOT NULL,
     role         ENUM ('ADMIN', 'USER')                           NOT NULL,
     status       ENUM ('ACTIVE', 'BLOCKED', 'DELETED', 'PENDING') NOT NULL,
-    username     VARCHAR(255)                                     NOT NULL UNIQUE
+    created_at   DATETIME(6),
+    deleted_at   DATETIME(6)
 );
 
 -- Card Table
@@ -26,10 +26,10 @@ CREATE TABLE card
     card_number     VARCHAR(255)         NOT NULL UNIQUE,
     created_at      DATETIME(6)          NOT NULL,
     cvv             VARCHAR(5)           NOT NULL,
-    deleted_at      DATETIME(6),
+    owner_id        UUID                 NOT NULL,
     expiration_date VARCHAR(10)          NOT NULL,
     is_deleted      TINYINT(1) DEFAULT 0 NOT NULL,
-    owner_id        UUID                 NOT NULL,
+    deleted_at      DATETIME(6),
     FOREIGN KEY (owner_id) REFERENCES user (id)
 );
 
@@ -37,10 +37,10 @@ CREATE TABLE card
 CREATE TABLE email_confirmation_token
 (
     id           UUID PRIMARY KEY,
+    user_id      UUID        NOT NULL,
     confirmed_at DATETIME(6),
     created_at   DATETIME(6) NOT NULL,
     expires_at   DATETIME(6) NOT NULL,
-    user_id      UUID        NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
@@ -48,12 +48,12 @@ CREATE TABLE email_confirmation_token
 CREATE TABLE wallet
 (
     id         UUID PRIMARY KEY,
-    balance    DECIMAL(38, 2)             NOT NULL,
-    created_at DATETIME(6)                NOT NULL,
-    currency   ENUM ('BGN', 'EUR', 'USD') NOT NULL,
+    balance    DECIMAL(38, 2)                                                     NOT NULL,
+    currency   ENUM ('BGN', 'EUR', 'USD','GBP', 'JPY', 'CNH','AUD', 'CAD', 'CHF') NOT NULL,
+    owner_id   UUID                                                               NOT NULL,
+    created_at DATETIME(6)                                                        NOT NULL,
     deleted_at DATETIME(6),
-    is_deleted TINYINT(1) DEFAULT 0       NOT NULL,
-    owner_id   UUID                       NOT NULL,
+    is_deleted TINYINT(1) DEFAULT 0                                               NOT NULL,
     FOREIGN KEY (owner_id) REFERENCES user (id)
 );
 
@@ -61,15 +61,15 @@ CREATE TABLE wallet
 CREATE TABLE exchange
 (
     id                 UUID PRIMARY KEY,
-    amount             DECIMAL(38, 2)             NOT NULL,
-    to_amount          DECIMAL(38, 2)             NOT NULL,
-    exchange_rate      DECIMAL(38, 10)            NOT NULL,
-    from_currency      ENUM ('BGN', 'EUR', 'USD') NOT NULL,
-    to_currency        ENUM ('BGN', 'EUR', 'USD') NOT NULL,
-    recipient_username VARCHAR(255)               NOT NULL,
-    from_wallet_id     UUID                       NOT NULL,
-    to_wallet_id       UUID                       NOT NULL,
-    date               DATETIME(6)                NOT NULL,
+    amount             DECIMAL(38, 2)                                                 NOT NULL,
+    to_amount          DECIMAL(38, 2)                                                 NOT NULL,
+    exchange_rate      DECIMAL(38, 10)                                                NOT NULL,
+    from_currency      ENUM ('BGN', 'EUR', 'USD','GBP','JPY','CNH','AUD','CAD','CHF') NOT NULL,
+    to_currency        ENUM ('BGN', 'EUR', 'USD','GBP','JPY','CNH','AUD','CAD','CHF') NOT NULL,
+    recipient_username VARCHAR(255)                                                   NOT NULL,
+    from_wallet_id     UUID                                                           NOT NULL,
+    to_wallet_id       UUID                                                           NOT NULL,
+    date               DATETIME(6)                                                    NOT NULL,
     FOREIGN KEY (from_wallet_id) REFERENCES wallet (id),
     FOREIGN KEY (to_wallet_id) REFERENCES wallet (id)
 );
@@ -78,14 +78,14 @@ CREATE TABLE exchange
 CREATE TABLE transaction
 (
     id                  UUID PRIMARY KEY,
-    amount              DECIMAL(38, 2)             NOT NULL,
-    currency            ENUM ('BGN', 'EUR', 'USD') NOT NULL,
-    sender_username     VARCHAR(255)               NOT NULL,
-    recipient_username  VARCHAR(255)               NOT NULL,
-    description         VARCHAR(255)               NOT NULL,
-    date                DATETIME(6)                NOT NULL,
-    recipient_wallet_id UUID                       NOT NULL,
-    sender_wallet_id    UUID                       NOT NULL,
+    amount              DECIMAL(38, 2)                                                 NOT NULL,
+    currency            ENUM ('BGN', 'EUR', 'USD','GBP','JPY','CNH','AUD','CAD','CHF') NOT NULL,
+    sender_username     VARCHAR(255)                                                   NOT NULL,
+    recipient_username  VARCHAR(255)                                                   NOT NULL,
+    description         VARCHAR(255)                                                   NOT NULL,
+    date                DATETIME(6)                                                    NOT NULL,
+    recipient_wallet_id UUID                                                           NOT NULL,
+    sender_wallet_id    UUID                                                           NOT NULL,
     FOREIGN KEY (sender_wallet_id) REFERENCES wallet (id),
     FOREIGN KEY (recipient_wallet_id) REFERENCES wallet (id)
 );
@@ -94,13 +94,13 @@ CREATE TABLE transaction
 CREATE TABLE transfer
 (
     id                 UUID PRIMARY KEY,
-    amount             DECIMAL(38, 2)                NOT NULL,
-    currency           ENUM ('BGN', 'EUR', 'USD')    NOT NULL,
-    status             ENUM ('APPROVED', 'DECLINED') NOT NULL,
-    recipient_username VARCHAR(255)                  NOT NULL,
-    date               DATETIME(6)                   NOT NULL,
-    card_id            UUID                          NOT NULL,
-    wallet_id          UUID                          NOT NULL,
+    amount             DECIMAL(38, 2)                                                 NOT NULL,
+    currency           ENUM ('BGN', 'EUR', 'USD','GBP','JPY','CNH','AUD','CAD','CHF') NOT NULL,
+    status             ENUM ('APPROVED', 'DECLINED')                                  NOT NULL,
+    recipient_username VARCHAR(255)                                                   NOT NULL,
+    date               DATETIME(6)                                                    NOT NULL,
+    card_id            UUID                                                           NOT NULL,
+    wallet_id          UUID                                                           NOT NULL,
     FOREIGN KEY (card_id) REFERENCES card (id),
     FOREIGN KEY (wallet_id) REFERENCES wallet (id)
 );
@@ -109,8 +109,7 @@ CREATE TABLE transfer
 CREATE TABLE exchange_rate
 (
     id            UUID PRIMARY KEY,
-    rate          DECIMAL(38, 10)            NOT NULL,
-    from_currency ENUM ('BGN', 'EUR', 'USD') NOT NULL,
-    to_currency   ENUM ('BGN', 'EUR', 'USD') NOT NULL
+    rate          DECIMAL(38, 10)                                                NOT NULL,
+    from_currency ENUM ('BGN', 'EUR', 'USD','GBP','JPY','CNH','AUD','CAD','CHF') NOT NULL,
+    to_currency   ENUM ('BGN', 'EUR', 'USD','GBP','JPY','CNH','AUD','CAD','CHF') NOT NULL
 );
-
