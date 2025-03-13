faf3ed83effac61db22d84cf3574535c0e887b2d
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-13 18:41:51 +0200

    changed min name requirements

--- src/main/java/com/example/virtualwallet/models/dtos/user/ProfileUpdateInput.java
+++ src/main/java/com/example/virtualwallet/models/dtos/user/ProfileUpdateInput.java
@@ -11,10 +11,10 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:messages.properties")
 public class ProfileUpdateInput {
 
-    @Size(min = 4, max = 32, message = "{error.firstNameLength}")
+    @Size(min = 2, max = 32, message = "{error.firstNameLength}")
     private String firstName;
 
-    @Size(min = 4, max = 32, message = "{error.lastNameLength}")
+    @Size(min = 2, max = 32, message = "{error.lastNameLength}")
     private String lastName;
 
     @Email(message = "{error.emailInvalid}")
@@ -23,14 +23,4 @@ public class ProfileUpdateInput {
 
     @Pattern(regexp = "\\d{10}", message = "{error.phoneDigits}")
     private String phoneNumber;
-
-    @NotBlank(message = "{error.passwordRequired}")
-    private String password;
-
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[+\\-*&^%$#@!])[A-Za-z\\d+\\-*&^%$#@!]{8,}$",
-            message = "{error.passwordValidation}")
-    @Size(min = 8, max = 40, message = "{error.passwordLength}")
-    private String newPassword;
-
-    private String newPasswordConfirm;
 }
