593d3dfd8a252b918ee3ebf77950366853df597a
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-14 00:02:59 +0200

    update: fixed YearMonthSerializer

--- src/main/java/com/example/virtualwallet/helpers/YearMonthSerializer.java
+++ src/main/java/com/example/virtualwallet/helpers/YearMonthSerializer.java
@@ -1,17 +1,22 @@
 package com.example.virtualwallet.helpers;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
 import java.io.IOException;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 
-public class YearMonthSerializer extends JsonSerializer<YearMonth> {
+public class YearMonthSerializer extends StdSerializer<YearMonth> {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM/yy");
 
+    public YearMonthSerializer() {
+        super(YearMonth.class);
+    }
+
     @Override
-    public void serialize(YearMonth value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+    public void serialize(YearMonth value, JsonGenerator gen, SerializerProvider provider) throws IOException {
         gen.writeString(value.format(FORMATTER));
     }
 }
