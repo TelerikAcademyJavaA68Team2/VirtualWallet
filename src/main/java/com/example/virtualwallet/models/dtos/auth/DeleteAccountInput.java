6ec591974a35947861a3f5fb2a73ba405e99201e
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-13 18:44:58 +0200

    changed name for consistency

--- src/main/java/com/example/virtualwallet/models/dtos/auth/DeleteAccountInput.java
+++ src/main/java/com/example/virtualwallet/models/dtos/auth/DeleteAccountInput.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class DeleteAccountRequest {
+public class DeleteAccountInput {
 
     @NotBlank(message = "Wrong Password")
     private String password;
