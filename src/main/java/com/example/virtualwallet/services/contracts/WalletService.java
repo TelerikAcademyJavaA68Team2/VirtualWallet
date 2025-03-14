1825975bafbebf7fe71281899a337121e08e541b
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-14 16:34:58 +0200

    changed the return type of getAllActiveWallets

--- src/main/java/com/example/virtualwallet/services/contracts/WalletService.java
+++ src/main/java/com/example/virtualwallet/services/contracts/WalletService.java
@@ -1,11 +1,10 @@
 package com.example.virtualwallet.services.contracts;
 
 import com.example.virtualwallet.models.Wallet;
-import com.example.virtualwallet.models.dtos.wallet.WalletBasicOutput;
 import com.example.virtualwallet.models.dtos.pageable.WalletPageOutput;
+import com.example.virtualwallet.models.dtos.wallet.WalletsWithHistoryOutput;
 import com.example.virtualwallet.models.enums.Currency;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface WalletService {
@@ -16,7 +15,7 @@ public interface WalletService {
 
     void update(Wallet wallet);
 
-    List<WalletBasicOutput> getActiveWalletsOfAuthenticatedUser();
+    WalletsWithHistoryOutput getActiveWalletsOfAuthenticatedUser(String mainCurrency, int page, int size);
 
     WalletPageOutput getWalletPageById(String currency, int page, int size);
 
