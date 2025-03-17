58d5de2ebfcbe0d0a2c5aef20d4a1e66c5c07c6b
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-17 18:08:21 +0200

    added single wallet endpoint

--- src/main/java/com/example/virtualwallet/controllers/mvc/WalletMvcController.java
+++ src/main/java/com/example/virtualwallet/controllers/mvc/WalletMvcController.java
@@ -1,19 +1,21 @@
 package com.example.virtualwallet.controllers.mvc;
 
 import com.example.virtualwallet.helpers.ValidationHelpers;
+import com.example.virtualwallet.models.dtos.pageable.WalletPageOutput;
 import com.example.virtualwallet.models.dtos.wallet.WalletBasicOutput;
 import com.example.virtualwallet.models.dtos.wallet.WalletsWithHistoryOutput;
-import com.example.virtualwallet.services.contracts.ExchangeRateService;
 import com.example.virtualwallet.services.contracts.UserService;
 import com.example.virtualwallet.services.contracts.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.UUID;
 
 import static com.example.virtualwallet.helpers.ValidationHelpers.requestIsWithInvalidPageOrSize;
 
@@ -24,7 +26,6 @@ public class WalletMvcController {
 
     private final WalletService walletService;
     private final UserService userService;
-    private final ExchangeRateService exchangeRateService;
 
     @GetMapping
     public String getWallets(@RequestParam(defaultValue = "BGN") String mainCurrency,
@@ -46,7 +47,22 @@ public class WalletMvcController {
         model.addAttribute("walletCurrencies", walletCurrencies);
         model.addAttribute("currentUserUsername", userService.getAuthenticatedUser().getUsername());
         return "Wallets-View";
+    }
 
+    @GetMapping("/{walletId}")
+    public String getWalletPageById(@PathVariable UUID walletId,
+                                    @RequestParam(defaultValue = "0") int page,
+                                    @RequestParam(defaultValue = "10") int size,
+                                    Model model) {
+        if (requestIsWithInvalidPageOrSize(page, size)) {
+            page = 0;
+            size = 10;
+        }
+        WalletPageOutput output = walletService.getWalletPageById(walletId, page, size);
+
+        model.addAttribute("wallet", output);
+        model.addAttribute("currentUserUsername", userService.getAuthenticatedUser().getUsername());
+        return "Wallet-View";
     }
 
 
