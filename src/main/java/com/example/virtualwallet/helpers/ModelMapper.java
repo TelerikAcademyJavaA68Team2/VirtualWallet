5e176f74673802278521bf9a70ecd37718cac75e
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-23 01:36:01 +0200

    bugfix: UUID parsing in ModelMapper

--- src/main/java/com/example/virtualwallet/helpers/ModelMapper.java
+++ src/main/java/com/example/virtualwallet/helpers/ModelMapper.java
@@ -201,7 +201,7 @@ public class ModelMapper {
 
     public static ActivityOutput mapObjectToActivity(Object[] row) {
         return new ActivityOutput(
-                UUID.fromString((String) row[0]),
+                UUID.fromString(row[0].toString()),
                 (String) row[1],
                 (BigDecimal) row[2],
                 (BigDecimal) row[3],
