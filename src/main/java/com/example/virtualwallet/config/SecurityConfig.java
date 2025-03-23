d2b65b27bc83b91438808864432984be4e655655
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-23 14:50:23 +0200

    update: successful login now redirects to wallets
    (cherry picked from commit c1926c921c5f85f457296f33e1ce8331e10c933b)

--- src/main/java/com/example/virtualwallet/config/SecurityConfig.java
+++ src/main/java/com/example/virtualwallet/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
             HttpSession session = request.getSession();
             User user = (User) authentication.getPrincipal();
             session.setAttribute("currentUser", user.getUsername());
-            response.sendRedirect("/mvc/home");
+            response.sendRedirect("/mvc/profile/wallets");
         };
     }
 
