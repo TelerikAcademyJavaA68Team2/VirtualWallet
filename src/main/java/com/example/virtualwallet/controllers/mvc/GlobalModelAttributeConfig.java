da593c468c94d868b7b54369821a79dfc53c1560
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-25 02:26:57 +0200

    update: added GlobalModelAttributeConfig and Page-Layout.html for current user model attribute for dynamic profile pic update

---
+++ src/main/java/com/example/virtualwallet/controllers/mvc/GlobalModelAttributeConfig.java
@@ -0,0 +1,26 @@
+package com.example.virtualwallet.controllers.mvc;
+
+import com.example.virtualwallet.exceptions.UnauthorizedAccessException;
+import com.example.virtualwallet.models.User;
+import com.example.virtualwallet.services.contracts.UserService;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice(basePackages = "com.example.virtualwallet.controllers.mvc")
+public class GlobalModelAttributeConfig {
+
+    private final UserService userService;
+
+    public GlobalModelAttributeConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @ModelAttribute("currentUser")
+    public User addUserToModel() {
+        try {
+            return userService.getAuthenticatedUser();
+        } catch (UnauthorizedAccessException e) {
+            return null;
+        }
+    }
+}
