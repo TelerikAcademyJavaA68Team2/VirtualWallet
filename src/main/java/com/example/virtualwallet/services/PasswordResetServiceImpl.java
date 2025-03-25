21c9c1987cc36c887db6577cbae165d0bd21da83
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-25 20:10:11 +0200

    implemented ResetPassword functionality
    (cherry picked from commit 25fc44b538dc794971552abaaf505e95400105a3)

---
+++ src/main/java/com/example/virtualwallet/services/PasswordResetServiceImpl.java
@@ -0,0 +1,63 @@
+package com.example.virtualwallet.services;
+
+import com.example.virtualwallet.exceptions.EmailConfirmationException;
+import com.example.virtualwallet.exceptions.EntityNotFoundException;
+import com.example.virtualwallet.exceptions.InvalidUserInputException;
+import com.example.virtualwallet.models.ResetPasswordToken;
+import com.example.virtualwallet.models.User;
+import com.example.virtualwallet.models.dtos.auth.NewPasswordResetInput;
+import com.example.virtualwallet.models.dtos.auth.PasswordResetInput;
+import com.example.virtualwallet.repositories.PasswordResetRepository;
+import com.example.virtualwallet.services.contracts.EmailService;
+import com.example.virtualwallet.services.contracts.PasswordResetService;
+import com.example.virtualwallet.services.contracts.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordResetServiceImpl implements PasswordResetService {
+
+    private final PasswordResetRepository resetRepository;
+    private final EmailService emailService;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void sendResetPasswordEmail(PasswordResetInput input, boolean isRestRequest) throws EntityNotFoundException {
+
+        User user = userService.findUserByUsernameOrEmailOrPhoneNumber(input.getEmail());
+        if (resetRepository.checkIfTokenWasAlreadySent(user.getEmail(), LocalDateTime.now())) {
+            throw new EmailConfirmationException("The reset password email was already send!");
+        }
+
+        UUID tokenId = UUID.randomUUID();
+        ResetPasswordToken token = new ResetPasswordToken(tokenId, user);
+        resetRepository.save(token);
+
+        emailService.sendResetPasswordEmail(user.getFirstName(), user.getEmail(), tokenId.toString(), isRestRequest);
+    }
+
+    @Override
+    public void processResetPasswordInput(NewPasswordResetInput input, UUID tokenId) {
+        if (!input.getPassword().equals(input.getPasswordConfirm())) {
+            throw new InvalidUserInputException("Password confirmation failed");
+        }
+        ResetPasswordToken token = resetRepository.findById(tokenId).orElseThrow(() -> new EntityNotFoundException("The token is invalid"));
+
+        User user = token.getUser();
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+        userService.save(user);
+    }
+
+    @Override
+    public boolean checkIfTokenExists(UUID tokenId) {
+        ResetPasswordToken token = resetRepository.findById(tokenId).orElse(null);
+        return token != null && token.getConfirmedAt() == null && !token.getExpiresAt().isBefore(LocalDateTime.now());
+    }
+}
