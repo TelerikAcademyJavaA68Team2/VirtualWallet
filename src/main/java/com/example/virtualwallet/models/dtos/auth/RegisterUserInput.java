a477ad1b9bb745959eda89456a867a09d30c877a
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-03-25 23:44:36 +0200

    lowered password requirements
    (cherry picked from commit ff84a79c15dca87668636b465bb942c10a8dd66b)

--- src/main/java/com/example/virtualwallet/models/dtos/auth/RegisterUserInput.java
+++ src/main/java/com/example/virtualwallet/models/dtos/auth/RegisterUserInput.java
@@ -29,7 +29,7 @@ public class RegisterUserInput {
     private String username;
 
     @NotBlank(message = "{error.password}")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[+\\-*&^%$#@!])[A-Za-z\\d+\\-*&^%$#@!]{8,}$",
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{8,}$",
             message = "{error.passwordValidation}")
     @Size(min = 8, max = 40, message = "{error.passwordLength}")
     private String password;
