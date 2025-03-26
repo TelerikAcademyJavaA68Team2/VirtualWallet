ebf2da492983e52f8c07d8ca86b38e0a96491261
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-26 22:32:47 +0200

    bugfix: fixed contextLoads test not running properly and .env.test for testing variables

--- src/test/java/com/example/virtualwallet/VirtualWalletApplicationTests.java
+++ src/test/java/com/example/virtualwallet/VirtualWalletApplicationTests.java
@@ -1,10 +1,27 @@
 package com.example.virtualwallet;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VirtualWalletApplicationTests {
+
+    @BeforeAll
+    static void loadTestEnv() {
+        Dotenv dotenv = Dotenv.configure()
+                .filename(".env.test")
+                .ignoreIfMissing()
+                .load();
+
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
 
     @Test
     void contextLoads() {
